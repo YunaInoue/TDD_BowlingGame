7079c8fe46ef46db3dd6f054089fb827a07eb03a
YunaInoue <ynr.com1017@gmail.com>
2020-05-02 19:57:35 +0900

    全ての投球で1ピンたけ倒した場合のテストを追加

--- test/BowlingGameTest.php
+++ test/BowlingGameTest.php
@@ -8,12 +8,24 @@ class BowlingGameTest extends TestCase
     /**
      * @test
      */
-    public function 全投球ガタ―の場合のテスト()
+    public function 全ての投球がガタ―()
     {
         $game = new BowlingGame();
         for ($i = 1; $i <= 20; $i++) {
             $game->recordShot(0);
         }
         $this->assertEquals(0, $game->score);
+    }
+
+    /**
+     * @test
+     */
+    public function 全ての投球で1ピンたけ倒した()
+    {
+        $game = new BowlingGame();
+        for ($i = 1; $i <= 20; $i++) {
+            $game->recordShot(1);
+        }
+        $this->assertEquals(20, $game->score);
     }
 }
