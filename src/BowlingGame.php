7a2813804a8a49e7b1821f668b23a2e7313aed64
YunaInoue <ynr.com1017@gmail.com>
2020-05-02 19:53:29 +0900

    scoreプロパティとrecordShot()を追加

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,13 +1,26 @@
 <?php
 namespace myApp;
 
+
 class BowlingGame
 {
+    /** @var int 都道府県等が定める 額 */
+    public $score;
 
     /**
      * BowlingGame constructor.
      */
     public function __construct()
     {
+
     }
+
+    /**
+     * @param int $pins
+     */
+    public function recordShot(int $pins): void
+    {
+        $this->score += $pins;
+    }
+
 }
