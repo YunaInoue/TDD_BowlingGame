4cb876bd82f92ee42f1988782a0f59204a10ac5f
YunaInoue <ynr.com1017@gmail.com>
2020-05-02 20:51:05 +0900

    スペアの計算方法を実装した

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,11 +1,18 @@
 <?php
+
 namespace myApp;
 
 
 class BowlingGame
 {
-    /** @var int 都道府県等が定める 額 */
+    /** @var int スコア */
     public $score;
+
+    /** @var int フレームの1投球目で倒したピン数 */
+    public $firstShotPins;
+
+    /** @var bool スペアフラグ */
+    public $spare;
 
     /**
      * BowlingGame constructor.
@@ -21,6 +28,34 @@ class BowlingGame
     public function recordShot(int $pins): void
     {
         $this->score += $pins;
+        // 各フレームの1投目の場合
+        if ($this->isFirstShotInFrame()) {
+            // 前回スペアだった場合
+            if ($this->spare) {
+                $this->score += $pins;
+            }
+            $this->firstShotPins = $pins;
+            return;
+        }
+        // 各フレームの2投目の場合
+        $this->spare = $this->isSpare($pins);
+        $this->firstShotPins = null;
     }
 
+    /**
+     * @return bool
+     */
+    private function isFirstShotInFrame(): bool
+    {
+        return is_null($this->firstShotPins);
+    }
+
+    /**
+     * @param int $pins
+     * @return bool
+     */
+    public function isSpare(int $pins): bool
+    {
+        return $pins + $this->firstShotPins === 10;
+    }
 }
