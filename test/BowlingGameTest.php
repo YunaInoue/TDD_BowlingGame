77ac11c0291063459ae63ac747f4fb836b5e7e91
YunaInoue <ynr.com1017@gmail.com>
2020-05-02 20:18:56 +0900

    【リファクタ】重複部分をメソッド抽出（recordManyShots）

--- test/BowlingGameTest.php
+++ test/BowlingGameTest.php
@@ -20,9 +20,7 @@ class BowlingGameTest extends TestCase
      */
     public function 全ての投球がガタ―()
     {
-        for ($i = 1; $i <= 20; $i++) {
-            $this->game->recordShot(0);
-        }
+        $this->recordManyShots(20, 0);
         $this->assertEquals(0, $this->game->score);
     }
 
@@ -31,9 +29,18 @@ class BowlingGameTest extends TestCase
      */
     public function 全ての投球で1ピンたけ倒した()
     {
-        for ($i = 1; $i <= 20; $i++) {
-            $this->game->recordShot(1);
-        }
+        $this->recordManyShots(20, 1);
         $this->assertEquals(20, $this->game->score);
+    }
+
+    /**
+     * @param int $count
+     * @param int $pins
+     */
+    private function recordManyShots(int $count, int $pins): void
+    {
+        for ($i = 1; $i <= $count; $i++) {
+            $this->game->recordShot($pins);
+        }
     }
 }
