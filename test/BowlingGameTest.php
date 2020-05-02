f57fcd520bfe171c795c07ac0db1334741a94796
YunaInoue <ynr.com1017@gmail.com>
2020-05-02 17:44:14 +0900

    BowlingGameテストクラスの作成

---
+++ test/BowlingGameTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class BowlingGameTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function 全投球ガタ―の場合のテスト()
+    {
+        $game = new BowlingGame();
+        $this->assertTrue(true);
+    }
+}
