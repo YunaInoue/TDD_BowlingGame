37b060cccbd47abdff86e44b053a709bc5a2130e
YunaInoue <ynr.com1017@gmail.com>
2020-05-03 11:59:00 +0900

    投球数プロパティとフレーム完了判定メソッドを追加した

--- src/Frame.php
+++ src/Frame.php
@@ -10,14 +10,17 @@ class Frame
     /** @var int スコア */
     public $score;
 
+    /** @var int 投球数 */
+    public $shotNo;
+
     /**
      * Frame constructor.
      */
     public function __construct()
     {
         $this->score = 0;
+        $this->shotNo = 0;
     }
-
 
     /**
      * @param int $pins
@@ -25,5 +28,14 @@ class Frame
     public function recordShot(int $pins): void
     {
         $this->score += $pins;
+        $this->shotNo += 1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function finished(): bool
+    {
+        return $this->shotNo === 2;
     }
 }
